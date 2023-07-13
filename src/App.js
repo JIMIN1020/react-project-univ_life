5307d2bca1f041a5b3e9dbf2bd1d10995ce31752
채요원 <email address>
2023-07-13 19:15:14 +0900

    성적 UI 디자인

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import GradePage from "./Pages/GradePage";
 import GraduationPage from "./Pages/GraduationPage";
 import MainPage from "./Pages/MainPage";
 import Page from "./Pages/Page";
 
 function App() {
-  return <GraduationPage></GraduationPage>;
+  return <GradePage></GradePage>;
 }
 
 export default App;
