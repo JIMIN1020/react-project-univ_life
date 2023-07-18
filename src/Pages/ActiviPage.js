f67978c39d5dcb0fc4138b1edc0df1fe49944f39
Kim Eun Ji <dms2107@sookmyung.ac.kr>
2023-07-19 01:47:26 +0900

    Revert "20-활동_UI 디자인 추가"
    This reverts commit 17bde886e6b3660f3017ae88c6d6dfdc9413b6f1.

--- src/Pages/ActiviPage.js
+++ src/Pages/ActiviPage.js
@@ -1,20 +1,9 @@
-import React, {useState} from "react";
+import React from "react";
 import styles from "./ActiviPage.module.css";
 import Profile from "./Components/Profile";
 import IndexBar from "./Components/IndexBar";
-import ActiviModal from "./Components/ActiviPage/ActiviModal";
-import { BsPlusCircle } from "react-icons/bs";
 
 const GradePage = () => {
-  const [activi,setActivi] = useState([]);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [activiSelected, setActiviSelected] = useState({});
-
-  const handleClick = (activi) => {
-    setModalOpen(true);
-    setActiviSelected(activi);
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -24,43 +13,38 @@ const GradePage = () => {
         <IndexBar />
         <div className={styles.contents}>
           <Profile />
-          <div class={styles.contentContainer}>
-            <div class={styles.section}></div>
-            <div class={styles.section}></div>
-          </div>
           <div className={styles.contentBox}>
-            <div className={styles.contentTop}>
-              <div className={styles.contenth}>동아리<br/>&<br/>학회</div>
-              <div className={styles.contentBottom}>
-              <button
-                      className={styles.addBtn}
-                      onClick={()=>handleClick(activiSelected)}
-                    >
-                      <BsPlusCircle style={{ height: "30px", width: "30px" }} />
-                    </button>
+            <h3>활동관리</h3>
+            <div className={styles.content}>
+            <h3>1</h3>
+              <div className={styles.div1}>
+              </div>
+              <div className={styles.div1}>
+              </div>
+              <div className={styles.div1}>
               </div>
             </div>
-            <div className={styles.divider}></div>
-            <div className={styles.contentTop}>
-              <div className={styles.contenth}>대외활동</div>
-              <div className={styles.contentBottom}>
-                <div className={styles.div}></div>
+            <div className={styles.content}>
+            <h3>2</h3>
+              <div className={styles.div1}>
+              </div>
+              <div className={styles.div1}>
+              </div>
+              <div className={styles.div1}>
               </div>
             </div>
-            <div className={styles.divider}></div>
-            <div className={styles.contentTop}>
-              <div className={styles.contenth}>프로젝트</div>
-              <div className={styles.contentBottom}>
-                <div className={styles.div}></div>
-                <div className={styles.div}></div>
+            <div className={styles.content}>
+            <h3>3</h3>
+              <div className={styles.div1}>
+              </div>
+              <div className={styles.div1}>
+              </div>
+              <div className={styles.div1}>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {modalOpen && (
-        <ActiviModal {...activiSelected} setModalOpen={setModalOpen} />
-      )}
     </div>
   );
 };
