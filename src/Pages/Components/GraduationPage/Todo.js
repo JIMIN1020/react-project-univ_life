a0ef8a3347ededb0c86412a20ee94cc30ae7a85d
JIMIN1020 <jimins4920@gmail.com>
2023-07-18 17:28:47 +0900

    9-졸업_todo 디자인 & 기능

--- src/Pages/Components/GraduationPage/Todo.js
+++ src/Pages/Components/GraduationPage/Todo.js
@@ -1,15 +1,34 @@
 import React from "react";
 import styles from "./Todo.module.css";
+import { BsCheckSquare, BsCheckSquareFill, BsXLg } from "react-icons/bs";
 
-const Todo = () => {
+const Todo = ({ todo, completeTodo, deleteTodo }) => {
   return (
     <div className={styles.container}>
       <div>
-        <input type="checkbox" />
-        <span>todo</span>
+        <label>
+          <input
+            className={styles.checkbox}
+            type="checkbox"
+            onChange={() => completeTodo(todo.id)}
+          />
+          {todo.completed ? <BsCheckSquareFill /> : <BsCheckSquare />}
+        </label>
+
+        <span
+          style={
+            todo.completed ? { textDecoration: "line-through" } : undefined
+          }
+        >
+          {todo.value}
+        </span>
       </div>
-      <button className={styles.button} type="submit">
-        X
+      <button
+        className={styles.button}
+        type="submit"
+        onClick={() => deleteTodo(todo.id)}
+      >
+        <BsXLg />
       </button>
     </div>
   );
