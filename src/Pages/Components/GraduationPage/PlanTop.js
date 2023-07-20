d2d6582d69b6076dd944c6ddab6b9cc5540827a8
JIMIN1020 <jimins4920@gmail.com>
2023-07-20 19:42:59 +0900

    33-졸업_계획 모달 기능

--- src/Pages/Components/GraduationPage/PlanTop.js
+++ src/Pages/Components/GraduationPage/PlanTop.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./PlanTop.module.css";
+import { BsRecordFill } from "react-icons/bs";
 
-const PlanTop = () => {
+const PlanTop = ({ plan }) => {
+  const [modalOpen, setModalOpen] = useState(false); // 모달창 오픈 관리
   return (
     <div className={styles.container}>
       <div className={styles.planBox}>
-        <div className={styles.plan}></div>
+        <div className={styles.plan}>
+          <div className={styles.line}>
+            <BsRecordFill className={styles.icon} />
+            <span>토익 900점 넘기</span>
+          </div>
+          <div className={styles.line}>
+            <BsRecordFill className={styles.icon} />
+            <span>컴활 자격증 따기</span>
+          </div>
+          <div className={styles.line}>
+            <BsRecordFill className={styles.icon} />
+            <span>컴활 자격증 따기</span>
+          </div>
+        </div>
       </div>
       <div className={styles.lines}>
         <div className={styles.leftline}></div>
         <div className={styles.circle}></div>
         <div className={styles.rightline}></div>
       </div>
-      <h3>2022학년도 2학기</h3>
+      <h3>{plan.title}</h3>
     </div>
   );
 };
