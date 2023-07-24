b53cf3cea852f832d8b3477ee0fd0e791b29d735
JIMIN1020 <jimins4920@gmail.com>
2023-07-24 15:15:16 +0900

    38-메인_UI 디자인

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import GradePage from "./Pages/GradePage";
 import GraduationPage from "./Pages/GraduationPage";
 import ActiviPage from "./Pages/ActiviPage";
 import MainPage from "./Pages/MainPage";
-import Page from "./Pages/Page";
 import BlogPage from "./Pages/BlogPage";
 
 function App() {
   return (
     <>
+      <MainPage />
       <GradePage />
       <ActiviPage />
       <GraduationPage />
