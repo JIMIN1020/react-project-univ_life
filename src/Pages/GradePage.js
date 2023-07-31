b8b5307c50a4d35bca0c01d32f4fbe8d309e0358
채요원 <email address>
2023-08-01 07:08:08 +0900

    68-성적 차트 기능

--- src/Pages/GradePage.js
+++ src/Pages/GradePage.js
@@ -12,6 +12,73 @@ import Box7 from "../Components/GradePage/Box7";
 import Box8 from "../Components/GradePage/Box8";
 import Box9 from "../Components/GradePage/Box9";
 import { Link } from "react-router-dom";
+import { ResponsiveLine } from '@nivo/line';
+
+const MyResponsiveLine = ({ data }) => (
+  <ResponsiveLine
+    data={data}
+    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+    xScale={{ type: 'point' }}
+    yScale={{
+      type: 'linear',
+      min: 'auto',
+      max: 'auto',
+      stacked: true,
+      reverse: false
+    }}
+    yFormat=" >-.2f"
+    axisTop={null}
+    axisRight={null}
+    axisBottom={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'Your Grade',
+      legendOffset: 36,
+      legendPosition: 'middle'
+    }}
+    axisLeft={{
+      tickSize: 5,
+      tickPadding: 5,
+      tickRotation: 0,
+      legend: 'count',
+      legendOffset: -40,
+      legendPosition: 'middle'
+    }}
+    pointSize={10}
+    pointColor={{ theme: 'background' }}
+    pointBorderWidth={2}
+    pointBorderColor={{ from: 'serieColor' }}
+    pointLabelYOffset={-12}
+    useMesh={true}
+    legends={[
+      {
+        anchor: 'bottom-right',
+        direction: 'column',
+        justify: false,
+        translateX: 100,
+        translateY: 0,
+        itemsSpacing: 0,
+        itemDirection: 'left-to-right',
+        itemWidth: 80,
+        itemHeight: 20,
+        itemOpacity: 0.75,
+        symbolSize: 12,
+        symbolShape: 'circle',
+        symbolBorderColor: 'rgba(0, 0, 0, .5)',
+        effects: [
+          {
+            on: 'hover',
+            style: {
+              itemBackground: 'rgba(0, 0, 0, .03)',
+              itemOpacity: 1
+            }
+          }
+        ]
+      }
+    ]}
+  />
+);
 
 const TodoItem = ({ text, isChecked, onCheck }) => {
   return (
@@ -52,6 +119,47 @@ const GradePage = () => {
   const [addButtonClickCount, setAddButtonClickCount] = useState(0);
   const div6Ref = useRef(null);
   const contentBoxRef = useRef(null);
+
+  const data = [
+    {
+      "id": "grade",
+      "color": "hsl(318, 70%, 50%)",
+      "data": [
+        {
+          "x": "1학기",
+          "y": 4.5
+        },
+        {
+          "x": "2학기",
+          "y": 3.3
+        },
+        {
+          "x": "3학기",
+          "y": 3.6
+        },
+        {
+          "x": "4학기",
+          "y": 4.1
+        },
+        {
+          "x": "5학기",
+          "y": 3.0
+        },
+        {
+          "x": "6학기",
+          "y": 4.1
+        },
+        {
+          "x": "7학기",
+          "y": 3.8
+        },
+        {
+          "x": "8학기",
+          "y": 3.8
+        }
+      ]
+    }
+  ];
 
   useEffect(() => {
     const handleResize = () => {
@@ -147,38 +255,7 @@ const GradePage = () => {
 
     return totalCredits;
   };
-
-  const calculateGPA = () => {
-    const subjects = [
-      ...div2Contents,
-      ...div3Contents,
-      ...div4Contents,
-      ...div5Contents,
-      ...div6Contents,
-      ...div7Contents,
-      ...div8Contents,
-      ...div9Contents,
-    ];
-
-    let totalGradePoints = 0;
-    let totalCredits = 0;
-
-    for (const subject of subjects) {
-      const credit = parseInt(subject.credit);
-      const grade = parseFloat(subject.grade);
-
-      totalGradePoints += credit * grade;
-      totalCredits += credit;
-    }
-
-    if (totalCredits === 0) {
-      return 0;
-    }
-
-    const gpa = totalGradePoints / totalCredits;
-    return gpa.toFixed(2);
-  };
-
+  
   const swipeHandlers = useSwipeable({
     onSwipedUp: () => {
       if (div6Ref.current) {
@@ -251,10 +328,10 @@ const GradePage = () => {
                   </div>
                 </div>
                 <div className={styles.div10}>
-                  <h3>열람실 현황</h3>
-                  <p>5층 상상라운지 - 잔여좌석: 21</p>
-                  <p>5층 C.C Plaza - 잔여좌석: 52</p>
-                  <p>5층 송영숙미디어랩 - 잔여좌석: 0</p>
+                  <h3>성적 그래프</h3>
+                  <div style={{ height: "370px",width: "100%", marginTop: "-50px" }}>
+                    <MyResponsiveLine data={data} />
+                   </div>
                 </div>
                 <div className={styles.div1Contents}>
                   <div className={styles.label}>
