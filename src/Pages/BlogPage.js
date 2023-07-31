93037886ba8e924e4164a385cba4bdf05b778279
Kim Eun Yeong <iris578@naver.com>
2023-08-01 04:48:31 +0900

    66-블로그_글 수정 및 삭제 기능

--- src/Pages/BlogPage.js
+++ src/Pages/BlogPage.js
@@ -8,29 +8,59 @@ import NewBlogModal from "./Components/BlogPage/NewBlogModal";
 import { Link } from "react-router-dom";
 
 export const BlogPage = () => {
-
   const initialBlogData = localStorage.getItem("blogData")
-  ? JSON.parse(localStorage.getItem("blogData")) : [];
+    ? JSON.parse(localStorage.getItem("blogData"))
+    : [];
+
+  const options = [
+    { value: "daily", name: "일상" },
+    { value: "memo", name: "메모" },
+    { value: "etc", name: "기타" },
+  ];
 
   const [blogData, setBlogData] = useState(initialBlogData);
   const [modalOpen, setModalOpen] = useState(false);
   const [newModalOpen, setNewModalOpen] = useState(false);
   const [blogSelected, setBlogSelected] = useState({});
 
+
+  const blogClicked = (blog) => {
+    console.log("this is blogpage hey: " + blogSelected.title + blogData);
+  }
+
   const showModal = (blog) => {
     setModalOpen(true);
     setBlogSelected(blog);
-  }
+    console.log("this is blogpage : " + blogSelected.title + blogData);
+  };
 
   const showNewModal = () => {
     setNewModalOpen(true);
-  }
+  };
 
   const addNewBlog = (newBlog) => {
     setBlogData((prev) => [...prev, newBlog]);
     localStorage.setItem("blogData", JSON.stringify([...blogData, newBlog]));
-  }
-  
+  };
+
+  // 블로그 수정
+  const handleEditSubmit = (id, editedTitle, editedType, editedContent) => {
+    const editedBlogData = blogData.map((blog) => {
+      if (blog.id === id) {
+        return {
+          ...blog,
+          title: editedTitle,
+          type: editedType,
+          content: editedContent,
+        };
+      }
+      return blog;
+    });
+    setBlogData(editedBlogData);
+    localStorage.setItem("blogData", JSON.stringify(editedBlogData));
+    setModalOpen(false);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -52,24 +82,47 @@ export const BlogPage = () => {
               <button className={styles.category_add}>+add</button>
             </div>
             <div>
-                <section className={styles.cardsList}>
-                  {blogData.map((blog) => (
-                    <Card
-                        key={blog.id}
-                        title={blog.title}
-                        type={blog.type}
-                        content={blog.content}
-                        setModalOpen={showModal}
-                      />
-                  ))}
-                  <section className="addcard" onClick={() => showNewModal()}>
-                    <div className={styles.card_add}>+</div>
-                  </section>
+              <section className={styles.cardsList}>
+                {blogData.map((blog) => (
+                  <Card
+                    id={blog.id}
+                    title={blog.title}
+                    type={blog.type}
+                    date={blog.date}
+                    content={blog.content}
+                    setModalOpen={showModal}
+                    setBlogSelected={setBlogSelected}
+                    onClick={() => blogClicked()}
+                  />
+                ))}
+                <section className="addcard" onClick={() => showNewModal()}>
+                  <div className={styles.card_add}>+</div>
                 </section>
+              </section>
             </div>
-            {modalOpen && <BlogModal blog={blogSelected} setModalOpen={setModalOpen} />}
-            {newModalOpen && <NewBlogModal setNewModalOpen={setNewModalOpen} addNewBlog={addNewBlog} />}
-          </div> 
+            {modalOpen && (
+              <BlogModal
+                id={blogSelected.id}
+                title={blogSelected.title}
+                date={blogSelected.date}
+                type={blogSelected.type}
+                content={blogSelected.content}
+                handleEditSubmit={handleEditSubmit}
+                setBlogSelected={setBlogSelected}
+                setBlogData={setBlogData}
+                setModalOpen={setModalOpen}
+                blogData={blogData}
+                options={options}
+              />
+            )}
+            {newModalOpen && (
+              <NewBlogModal
+                setNewModalOpen={setNewModalOpen}
+                addNewBlog={addNewBlog}
+                options={options}
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
