d6f093951e538d5496b3c1256b758165f25b43af
Kim Eun Ji <dms2107@sookmyung.ac.kr>
2023-07-31 17:15:59 +0900

    55-활동_모달과 div간 기능 추가

--- src/Pages/ActiviPage.js
+++ src/Pages/ActiviPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styles from "./ActiviPage.module.css";
-import Profile from "../Components/Profile"
+import Profile from "../Components/Profile";
 import IndexBar from "../Components/IndexBar";
 import ActiviModal from "../Components/ActiviPage/ActiviModal";
 import { BsPlusCircle } from 'react-icons/bs';
@@ -11,12 +11,9 @@ import 'swiper/css';
 const ActiviPage = () => {
   const [modalOpen, setModalOpen] = useState(false);
   const [activiSelected, setActiviSelected] = useState({});
-  const [savedActiviContent, setSavedActiviContent] = useState({});
-
+  
   // 각 섹션(contentTop)에 대한 별도의 상태 변수들
-  const [activi1, setActivi1] = useState([
-    { id: Date.now(), date: "2022년도 2학기", title : "공과대학 학생회 재정사무국" },
-  ]);
+  const [activi1, setActivi1] = useState([]);
   const [activi2, setActivi2] = useState([]);
   const [activi3, setActivi3] = useState([]);
 
@@ -48,44 +45,60 @@ const ActiviPage = () => {
     setActiviSelected({ type: "프로젝트" });
   };
 
-  const handleClick = (date, title) => {
+  const handleClick = (section, id) => {
     setModalOpen(true);
-    setActiviSelected({ type: "동아리 & 학회" }); // Assuming this is the default type for now
-    setSavedActiviContent({ date, title });
+    setActiviSelected({ type: section, id: id });
   };
 
   const handleSaveActiviContent = (content) => {
     setModalOpen(false);
-    
-    // activiSelected.type에 따라 상태를 업데이트
-    switch (activiSelected.type) {
-      case "동아리 & 학회":
-        setActivi1((prevActivi) => {
-          // prevActivi에서 변경된 내용이 있는 항목의 id를 찾음
-          const updatedIndex = prevActivi.findIndex((item) => item.id === savedActiviContent.id);
-          // 변경된 내용이 있는 경우 해당 항목을 업데이트하고 아닌 경우 그대로 두기
-          return prevActivi.map((item, index) => (index === updatedIndex ? { ...item, ...content } : item));
-        });
-        break;
-      case "대외활동":
-        setActivi2((prevActivi) => {
-          // prevActivi에서 변경된 내용이 있는 항목의 id를 찾음
-          const updatedIndex = prevActivi.findIndex((item) => item.id === savedActiviContent.id);
-          // 변경된 내용이 있는 경우 해당 항목을 업데이트하고 아닌 경우 그대로 두기
-          return prevActivi.map((item, index) => (index === updatedIndex ? { ...item, ...content } : item));
-        });
-        break;
-      case "프로젝트":
-        setActivi3((prevActivi) => {
-          // prevActivi에서 변경된 내용이 있는 항목의 id를 찾음
-          const updatedIndex = prevActivi.findIndex((item) => item.id === savedActiviContent.id);
-          // 변경된 내용이 있는 경우 해당 항목을 업데이트하고 아닌 경우 그대로 두기
-          return prevActivi.map((item, index) => (index === updatedIndex ? { ...item, ...content } : item));
-        });
-        break;
-      default:
-        break;
+
+    // 선택된 div의 정보가 있는 경우에만 처리
+    if (activiSelected) {
+      const { type, id } = activiSelected;
+      const updatedContent = { ...content, id: Date.now() };
+
+      switch (type) {
+        case "동아리 & 학회":
+          setActivi1((prevActivi) => {
+            return prevActivi.map((item) => {
+              if (item.id === id) {
+                return updatedContent;
+              } else {
+                return item;
+              }
+            });
+          });
+          break;
+        case "대외활동":
+          setActivi2((prevActivi) => {
+            return prevActivi.map((item) => {
+              if (item.id === id) {
+                return updatedContent;
+              } else {
+                return item;
+              }
+            });
+          });
+          break;
+        case "프로젝트":
+          setActivi3((prevActivi) => {
+            return prevActivi.map((item) => {
+              if (item.id === id) {
+                return updatedContent;
+              } else {
+                return item;
+              }
+            });
+          });
+          break;
+        default:
+          break;
+      }
     }
+
+    // 선택 상태 초기화
+    setActiviSelected(null);
   };
 
   return (
@@ -99,9 +112,9 @@ const ActiviPage = () => {
         <IndexBar id={2} />
         <div className={styles.contents}>
           <Profile />
-          <div class={styles.contentContainer}>
-            <div class={styles.section}></div>
-            <div class={styles.section}></div>
+          <div className={styles.contentContainer}>
+            <div className={styles.section}></div>
+            <div className={styles.section}></div>
           </div>
           <div className={styles.contentBox}>
             <div className={styles.contentTop}>
@@ -110,106 +123,76 @@ const ActiviPage = () => {
                 &<br />
                 학회<br/>
                 <button className={styles.Btn} onClick={handleClick1}>
-                <BsPlusCircle style={{ height: "18px", width: "18px" }} />
-              </button>
+                  <BsPlusCircle style={{ height: "18px", width: "18px" }} />
+                </button>
               </div>
               
-              <Swiper
-                slidesPerView={3}
-                spaceBetween={10}
-                className={styles.contentBottom}
-              >
+              <Swiper slidesPerView={3} spaceBetween={10} className={styles.contentBottom}>
                 {activi1.map((item) => (
-                  <SwiperSlide key={item.id} className={styles.div} onClick={() => handleClick(item.date, item.title)}>
-                     {/* 내용이 저장되어 있으면 저장된 내용을 보여줌 */}
-                     {savedActiviContent && savedActiviContent.type === "동아리 & 학회" && (
-                      <>
-                        <p className={styles.dText}>{savedActiviContent.date}</p>
-                        <p className={styles.cText}>{savedActiviContent.title}</p>
-                      </>
-                      )}
-                      {!savedActiviContent && (
-                      <>
-                        <p className={styles.dText}>{item.date}</p>
-                        <p className={styles.cText}>{item.title}</p>
-                      </>
-                      )}
+                  <SwiperSlide
+                    key={item.id} 
+                    className={styles.div} 
+                    onClick={() => handleClick("동아리 & 학회", item.id)}
+                  >
+                    <p className={styles.dText}>{item.date}</p>
+                    <p className={styles.cText}>{item.title}</p>
                   </SwiperSlide>
                 ))}
-                
               </Swiper>
               {modalOpen && (
-                <ActiviModal
-                  {...activiSelected}
-                  setModalOpen={setModalOpen}
-                  onSave={handleSaveActiviContent}
-                  savedActiviContent={savedActiviContent}
-                  setSavedActiviContent={setSavedActiviContent}
-                />
-              )}
+        <ActiviModal
+          setModalOpen={setModalOpen}
+          onSave={handleSaveActiviContent}
+          savedActiviContent={
+            activiSelected.type === "동아리 & 학회"
+              ? activi1.find((item) => item.id === activiSelected.id)
+              : activiSelected.type === "대외활동"
+              ? activi2.find((item) => item.id === activiSelected.id)
+              : activi3.find((item) => item.id === activiSelected.id)
+          }
+        />
+      )}
+              
             </div>
             <div className={styles.divider}></div>
             <div className={styles.contentTop}>
               <div className={styles.contenth}>대외활동<br/>
-              <button className={styles.Btn} onClick={handleClick2}>
-                <BsPlusCircle style={{ height: "18px", width: "18px" }} />
-              </button>
+                <button className={styles.Btn} onClick={handleClick2}>
+                  <BsPlusCircle style={{ height: "18px", width: "18px" }} />
+                </button>
               </div>
-              <Swiper
-                slidesPerView={3}
-                spaceBetween={10}
-                className={styles.contentBottom}
-              >
+              <Swiper slidesPerView={3} spaceBetween={10} className={styles.contentBottom}>
                 {activi2.map((item) => (
-                  <SwiperSlide key={item.id} className={styles.div} onClick={() => handleClick(item.date, item.title)}>
-                     {/* 내용이 저장되어 있으면 저장된 내용을 보여줌 */}
-                     {savedActiviContent && savedActiviContent.type === "동아리 & 학회" && (
-                      <>
-                        <p className={styles.dText}>{savedActiviContent.date}</p>
-                        <p className={styles.cText}>{savedActiviContent.title}</p>
-                      </>
-                      )}
-                      {!savedActiviContent && (
-                      <>
-                        <p className={styles.dText}>{item.date}</p>
-                        <p className={styles.cText}>{item.title}</p>
-                      </>
-                      )}
+                  <SwiperSlide
+                    key={item.id} 
+                    className={styles.div} 
+                    onClick={() => handleClick("대외활동", item.id)}
+                  >
+                    <p className={styles.dText}>{item.date}</p>
+                    <p className={styles.cText}>{item.title}</p>
                   </SwiperSlide>
                 ))}
-                
               </Swiper>
             </div>
             <div className={styles.divider}></div>
             <div className={styles.contentTop}>
               <div className={styles.contenth}>프로젝트<br/>
-              <button className={styles.Btn} onClick={handleClick3}>
-                <BsPlusCircle style={{ height: "18px", width: "18px" }} />
-              </button>
+                <button className={styles.Btn} onClick={handleClick3}>
+                  <BsPlusCircle style={{ height: "18px", width: "18px" }} />
+                </button>
               </div>
-              <Swiper
-                slidesPerView={3}
-                spaceBetween={10}
-                className={styles.contentBottom}
-              >
+              <Swiper slidesPerView={3} spaceBetween={10} className={styles.contentBottom}>
                 {activi3.map((item) => (
-                  <SwiperSlide key={item.id} className={styles.div} onClick={() => handleClick(item.date, item.title)}>
-                     {/* 내용이 저장되어 있으면 저장된 내용을 보여줌 */}
-                     {savedActiviContent && savedActiviContent.type === "동아리 & 학회" && (
-                      <>
-                        <p className={styles.dText}>{savedActiviContent.date}</p>
-                        <p className={styles.cText}>{savedActiviContent.title}</p>
-                      </>
-                      )}
-                      {!savedActiviContent && (
-                      <>
-                        <p className={styles.dText}>{item.date}</p>
-                        <p className={styles.cText}>{item.title}</p>
-                      </>
-                      )}
+                  <SwiperSlide
+                    key={item.id} 
+                    className={styles.div} 
+                    onClick={() => handleClick("프로젝트", item.id)}
+                  >
+                    <p className={styles.dText}>{item.date}</p>
+                    <p className={styles.cText}>{item.title}</p>
+                    <p className={styles.cText}>{item.detail}</p>
                   </SwiperSlide>
                 ))}
-                
               </Swiper>
             </div>
           </div>
