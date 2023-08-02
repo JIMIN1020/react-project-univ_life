7f308303f89921eeb5559a346bb546302b81c573
Kim Eun Ji <dms2107@sookmyung.ac.kr>
2023-08-02 20:46:33 +0900

    81-활동_모달 수정 및 코드 정리

--- src/Components/ActiviPage/ActiviModal.js
+++ src/Components/ActiviPage/ActiviModal.js
@@ -3,16 +3,15 @@ import styles from "./ActiviModal.module.css";
 
 function ActiviModal({ setModalOpen, onSave, savedActiviContent }) {
   const [edit, setEdit] = useState(false);
-  const [date, setDate] = useState(savedActiviContent?.date || "학기");
-  const [title, setTitle] = useState(savedActiviContent?.title || "제목");
-  const [detail, setDetail] = useState(savedActiviContent?.content || "");
+  const [date, setDate] = useState(savedActiviContent?.date || "");
+  const [title, setTitle] = useState(savedActiviContent?.title || "");
+  const [detail, setDetail] = useState(savedActiviContent?.detail || "");
 
-  // Set the initial values based on the savedActiviContent prop
   useEffect(() => {
     if (savedActiviContent) {
-      setDate(savedActiviContent.date || "학기");
-      setTitle(savedActiviContent.title || "제목");
-      setDetail(savedActiviContent.content || "");
+      setDate(savedActiviContent.date || "");
+      setTitle(savedActiviContent.title || "");
+      setDetail(savedActiviContent.detail || "");
     }
   }, [savedActiviContent]);
 
@@ -34,11 +33,12 @@ function ActiviModal({ setModalOpen, onSave, savedActiviContent }) {
     setEdit(true);
   };
 
-  const handleSave = (date, title, content) => {
-    const savedContent = { date, title, content };
+  const handleSave = (date, title, detail) => {
+    const savedContent = { date, title, detail };
     onSave(savedContent);
     setModalOpen(false);
   };
+
 
   return (
     <div className={styles.modal}>
@@ -52,10 +52,11 @@ function ActiviModal({ setModalOpen, onSave, savedActiviContent }) {
             <input
               className={styles.modal__date}
               value={date}
+              placeholder="2000학년도 0학기"
               onChange={(e) => setDate(e.target.value)}
             />
           ) : (
-            <span className={styles.modal__date}>{date}</span>
+            <span className={styles.modal__date}>{date ? date : "학기"}</span>
           )}
 
           {edit ? (
@@ -63,21 +64,23 @@ function ActiviModal({ setModalOpen, onSave, savedActiviContent }) {
               className={styles.modal__title}
               style={{ width: "420px", height: "30px" }}
               value={title}
+              placeholder="제목"
               onChange={(e) => setTitle(e.target.value)}
             />
           ) : (
-            <h6 className={styles.modal__title}>{title}</h6>
+            <h6 className={styles.modal__title}>{title ? title : "제목"}</h6>
           )}
 
           {edit ? (
             <textarea
-              className={`${styles.modal__details} ${!detail && styles.hidden}`} /* detail이 비어있을 때 숨기기 */
+              className={styles.modal__details}
               style={{ width: "420px", height: "400px" }}
               value={detail}
+              placeholder="활동 내용을 입력하세요"
               onChange={(e) => setDetail(e.target.value)}
             ></textarea>
           ) : (
-            <p className={styles.modal__details}>{detail ? detail : "활동 내용이 비어있습니다."}</p>
+            <p className={styles.modal__details}>{detail ? detail : "활동"}</p>
           )}
         </div>
 
