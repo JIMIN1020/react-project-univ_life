6ebfcfb945395eae9b7a2e4ed866f01927b84e0f
JIMIN1020 <jimins4920@gmail.com>
2023-08-02 16:50:20 +0900

    79-프로필 데이터 업데이트

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
 import { FaHouse } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { authService, dbService } from "../fbase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 const Profile = () => {
+  const [profile, setProfile] = useState({});
+
+  /* ------------ 유저 정보 가져오기 ------------ */
+  const getUserInfo = async () => {
+    const q = query(
+      collection(dbService, "users"),
+      where("userID", "==", authService.currentUser.uid)
+    );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => setProfile(doc.data()));
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     <div className={styles.profile}>
       <div className={styles.userImage}>
@@ -15,15 +34,15 @@ const Profile = () => {
         />
       </div>
       <div className={styles.text1}>
-        눈송이
+        {profile.name}
         <br />
         2010.10.10
         <br />
       </div>
       <div className={styles.text2}>
         학교 | 숙명여자대학교 <br />
-        학번 | 200000 <br />
-        학과 | IT공학과
+        학번 | {profile.studentId} <br />
+        학과 | {profile.major}
       </div>
 
       <Link to="/" style={{ textDecoration: "none" }}>
